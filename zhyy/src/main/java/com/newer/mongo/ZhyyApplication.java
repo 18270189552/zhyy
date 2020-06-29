66ea4016d3439fb6b482265c9ba2ed367c44056b
Administrator <Administrator@BIDFD73G1V0UG4V>
2020-06-29 15:48:10 +0800

    项目初始化

---
+++ zhyy/src/main/java/com/newer/mongo/ZhyyApplication.java
@@ -0,0 +1,13 @@
+package com.newer.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhyyApplication.class, args);
+    }
+
+}
