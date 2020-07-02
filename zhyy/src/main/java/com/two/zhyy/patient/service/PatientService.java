78c8b748983ac531a09fc6bc1a9d0a4094ea8464
刘松杰 <@192.168.32.1>
2020-07-02 15:37:06 +0800

    修改

--- zhyy/src/main/java/com/two/zhyy/patient/service/PatientService.java
+++ zhyy/src/main/java/com/two/zhyy/patient/service/PatientService.java
@@ -1,13 +1,13 @@
 package com.two.zhyy.patient.service;
 
-import org.springframework.stereotype.Service;
+
 
 /**
  * 患者业务逻辑层
  * @author 小米
  *
  */
-@Service
+
 public interface PatientService {
 
 }
