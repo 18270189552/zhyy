8effb1ee42a72d24e0ec28237f022ee2926b1630
小米 <@DESKTOP-Q1EFK47>
2020-07-02 15:30:38 +0800

    王东（患者信息）

--- zhyy/src/main/java/com/two/zhyy/patient/service/PatientService.java
+++ zhyy/src/main/java/com/two/zhyy/patient/service/PatientService.java
@@ -1,5 +1,9 @@
 package com.two.zhyy.patient.service;
 
+import java.util.List;
+
+import com.two.zhyy.pojo.Reg;
+
 /**
  * 患者业务逻辑层
  * @author 小米
@@ -7,6 +11,7 @@ package com.two.zhyy.patient.service;
  */
 public interface PatientService {
 
-	
+	//定义获取指定患者病史
+	List<Reg> assfind();
 	
 }
--- zhyy/src/main/java/com/two/zhyy/patient/service/PatientServiceImpl.java
+++ zhyy/src/main/java/com/two/zhyy/patient/service/PatientServiceImpl.java
@@ -1,6 +1,10 @@
 package com.two.zhyy.patient.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.two.zhyy.pojo.Reg;
 
 /**
  * 业务逻辑的实现类
@@ -9,6 +13,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PatientServiceImpl implements PatientService{
+
+	@Override
+	public List<Reg> assfind() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 	
