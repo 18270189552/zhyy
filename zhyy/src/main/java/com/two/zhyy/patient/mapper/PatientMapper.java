e3aceb37670bf1d35b9a3c7d4d1c5a0b75f92410
小米 <@DESKTOP-Q1EFK47>
2020-07-02 15:16:17 +0800

    王东（业务）

---
+++ zhyy/src/main/java/com/two/zhyy/patient/mapper/PatientMapper.java
@@ -0,0 +1,13 @@
+package com.two.zhyy.patient.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 定义SQL与数据库的交互
+ * @author 小米
+ *
+ */
+@Mapper
+public interface PatientMapper {
+
+}
--- zhyy/src/main/java/com/two/zhyy/patient/mapper/patientMapper.java
+++
@@ -1,6 +0,0 @@
-package com.two.zhyy.patient.mapper;
-
-//定义SQL与数据库的交互
-public interface patientMapper {
-
-}
