cd735cb69113c01763ba38969823153fd6adcba2
小米 <@DESKTOP-Q1EFK47>
2020-07-02 15:20:44 +0800

    王东（业务2）

---
+++ zhyy/src/main/java/com/two/zhyy/patient/service/PatientServiceImpl.java
@@ -0,0 +1,15 @@
+package com.two.zhyy.patient.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 业务逻辑的实现类
+ * @author 小米
+ *
+ */
+@Service
+public class PatientServiceImpl {
+
+	
+	
+}
