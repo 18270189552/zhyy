a73faec5e24c545af08a678f6a8096e1da129c51
Administrator <Administrator@BIDFD73G1V0UG4V>
2020-07-02 11:09:05 +0800

    设置了应用程序的配置信息

---
+++ zhyy/src/main/java/com/two/zhyy/pojo/Working.java
@@ -0,0 +1,37 @@
+package com.two.zhyy.pojo;
+/**
+ * 排班实体类
+ * @author Administrator
+ *
+ */
+public class Working {
+	/**
+	 * 定义属性
+	 */
+	private Integer workid;  //排班id
+	private String data;  //排班日期
+	private  Integer number;  //最大诊断人数
+	
+	
+	public Integer getWorkid() {
+		return workid;
+	}
+	public void setWorkid(Integer workid) {
+		this.workid = workid;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	
+	
+}
