4342db2cabb2409da01d37824f76f31e6bfffa3b
汪怀杰 <@BIDFD73G1V0UG4V>
2020-07-02 16:45:53 +0800

    躲避spring安全的权限认证

--- zhyy/src/main/java/com/two/zhyy/ZhyyApplication.java
+++ zhyy/src/main/java/com/two/zhyy/ZhyyApplication.java
@@ -2,8 +2,9 @@ package com.two.zhyy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ZhyyApplication {
 	//asdf
     public static void main(String[] args) {
