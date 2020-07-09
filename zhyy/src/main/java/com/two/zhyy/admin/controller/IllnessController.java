d104bd1db99c08bb2f3bf5a9ea8c24c6bf3abba4
刘松杰 <@192.168.32.1>
2020-07-09 17:16:39 +0800

    刘松杰

--- zhyy/src/main/java/com/two/zhyy/admin/controller/IllnessController.java
+++ zhyy/src/main/java/com/two/zhyy/admin/controller/IllnessController.java
@@ -51,11 +51,6 @@ public class IllnessController {
 	public List<Illness> findSeid(@PathVariable int id) {
 		return illness.findBySeid(id);
 	}
-	//通过大科室名称查询科室信息
-	@GetMapping("/section")
-	public List<Illness> findName(@RequestParam("name") String name){
-		return illness.findName(name);
-	}
 	
 	//通过大科室查询子科室
 	@GetMapping("/section")
